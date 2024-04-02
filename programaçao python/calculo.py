201252df44cf9706e026d222282b15efa7dd4e21
wagnerAipol <wagclap@gmail.com>
2024-04-02 16:57:50 -0300

    criando arquivo calculo.py

---
+++ programaçao python/calculo.py
@@ -0,0 +1,7 @@
+print("3 + 2 = ", 3 + 2) # somando dois valores 
+print("3 - 2 = ", 3 - 2) # subtrai dois valores 
+print("3 * 2 = ", 3 * 2) # multiplica dois valores
+print("6 / 2 = ", 6 / 2) # divide dois valores
+print("6 % 2 = ", 6 % 2) # resto de uma divisao de dois valores
+print("3 / 2 = ", 3 / 2) # divisao de dois valores com resultado em ponto flutuante
+print("resultado de 2.5 + 1.1 = ", 2.5 + 1.1)
